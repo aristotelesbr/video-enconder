f1d3486cc9e03abb5c6875e77fec7d3411047fee
aristotelesbr <aristotelesbr@gmail.com>
2023-01-16 08:17:56 -0300

    chore: install GORM and add db config

---
+++ framework/database/db.go
@@ -0,0 +1,64 @@
+package database
+
+import (
+	"encoder/domain"
+	"log"
+
+	"github.com/jinzhu/gorm"
+)
+
+type Database struct {
+	Db          *gorm.DB
+	Dsn         string
+	DsnTest     string
+	DbType      string
+	DbTypeTest  string
+	Env         string
+	Debug       bool
+	AutoMigrate bool
+}
+
+func NewDb() *Database {
+	return &Database{}
+}
+
+func NewDbTest() *gorm.DB {
+	dbInstance := NewDb()
+	dbInstance.Env = "test"
+	dbInstance.DbType = "sqlite3"
+	dbInstance.Dsn = ":memory:"
+	dbInstance.AutoMigrate = true
+	dbInstance.Debug = true
+
+	connection, err := dbInstance.Connect()
+
+	if err != nil {
+		log.Fatalf("Test db error: %v", err)
+	}
+
+	return connection
+}
+
+func (db *Database) Connect() (*gorm.DB, error) {
+	var err error
+
+	if db.Env == "test" {
+		db.Db, err = gorm.Open(db.DbTypeTest, db.DsnTest)
+	} else {
+		db.Db, err = gorm.Open(db.DbType, db.Dsn)
+	}
+
+	if err != nil {
+		return nil, err
+	}
+
+	if db.Debug {
+		db.Db.LogMode(true)
+	}
+
+	if db.AutoMigrate {
+		db.Db.AutoMigrate(&domain.Video{}, &domain.Job{})
+	}
+
+	return db.Db, nil
+}
